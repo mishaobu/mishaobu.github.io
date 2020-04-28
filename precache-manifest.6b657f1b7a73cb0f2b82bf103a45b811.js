f2e63cf20386bfe24ad82f705b01f5fcccc77071
c1b <c1b@users.noreply.github.com>
2020-04-27 22:18:05 -0700

    Updates

---
+++ precache-manifest.6b657f1b7a73cb0f2b82bf103a45b811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1721bbf99a44dd64d56777171b0d7b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5fc73e61ac443c1a0a9",
+    "url": "/static/css/main.68481473.chunk.css"
+  },
+  {
+    "revision": "adc30d880c5713e388ea",
+    "url": "/static/js/2.6e454204.chunk.js"
+  },
+  {
+    "revision": "dd15359b453a3ff19ff7cf9a7f74ec93",
+    "url": "/static/js/2.6e454204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fc73e61ac443c1a0a9",
+    "url": "/static/js/main.58d8c85b.chunk.js"
+  },
+  {
+    "revision": "722b57f494fda0cb5b78",
+    "url": "/static/js/runtime-main.19e6cc94.js"
+  },
+  {
+    "revision": "1c6b1a9cd1468d6272ff6c43714331a0",
+    "url": "/static/media/band.1c6b1a9c.png"
+  }
+]);
